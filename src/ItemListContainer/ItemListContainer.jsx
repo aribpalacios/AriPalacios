1b2511f02c0e72858edc59c44d5e540a7eb25d7f
Ari Palacios <aribpalacios@gmail.com>
2021-11-13 17:10:46 -0300

    Se agrego navbar + item + itemlistcontainer

---
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,18 @@
+import Item from "../Item/Item";
+import "./ItemListContainer.css";
+
+export function ItemListaContainer () {
+
+
+
+  return(
+    <div className="item-list-container">
+      <Item name="Stag" price="150" imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgKLu3ajDUtX85RCUd5UFSjKt9bAwaKtWYBpkWZMAY3rw96naiczBkEkcw-uTaq2TPsu8&usqp=CAU  "  />
+      <Item name="Nordic" price="150" imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnBnMwdlvJ3fJc7TTs5RDnEjQ2Odx_FkXReSyWwfU6F0Y3nOvwPbwc-R0eOqBgc8YXHvE&usqp=CAU"/>
+      <Item name="Valhalla" price="150" imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShp4MFU9TAK959_0YuASgi1R3NCCFdq_WoQeVjepnTOuV9s5S0r7gS9Y5TqbVhCOzppDY&usqp=CAU" />
+     
+    </div>
+  )
+}
+
+export default ItemListaContainer;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+  return(
+    <nav>
+      <p> Cerveza en lata </p>
+      <p> Cerbeza en botellon</p>
+      <p> Vasos </p>
+
+    </nav>
+  )
+}
+
+export default NavBar
